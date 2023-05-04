30fd3ebb9a6732332f65efc7a52032a384b5cc6f
Shotokan ZH <shotokanzh@gmail.com>
2023-05-04 18:27:17 +0200

    scapy

--- vlanenumerator.py
+++ vlanenumerator.py
@@ -1,39 +1,21 @@
 #!/usr/bin/env python3
 import argparse
-import re
-import subprocess
 import os
 import threading
 import time
+from scapy.all import sniff, get_if_list, Dot1Q, packet
 
+validvlans = []
 
-def getInterfaces() -> list:
-    with open("/proc/net/dev", "r") as nd:
-        netdev = nd.read()
-    interfaces = []
-    for line in netdev.split("\n"):
-        m = re.search(f"^\s*([a-z0-9]+):", line, re.MULTILINE)
-        if m:
-            iface = m.group(1)
-            interfaces.append(iface)
-    return interfaces
-
-
-def capture(interface: str, validvlans: list):
-    p = subprocess.Popen(("tcpdump", "-i", interface, "-nn", "-e", "vlan"), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print("Waiting for VLANs to appear..")
-    reg = re.compile(r",\s+length \d+: vlan (\d+),")
-    for row in iter(p.stdout.readline, b''):
-        m = re.search(reg, row.decode())
-        if m:
-            vlanid = int(m.group(1))
-            if not vlanid in validvlans:
-                print("\tNew VLAN detected:", vlanid)
-                validvlans.append(vlanid)
-
+def capture(pkt:packet):
+    if pkt.haslayer(Dot1Q):
+        vlanid = pkt[Dot1Q].vlan
+        if not vlanid in validvlans:
+            print("\tNew VLAN detected:", vlanid)
+            validvlans.append(vlanid)
 
 def main():
-    ifaces = getInterfaces()
+    ifaces = get_if_list()
 
     parser = argparse.ArgumentParser(description="VlanEnumerator v1.0 By ShotokanZH")
     parser.add_argument("interface", choices=ifaces, help="Network interface to use")
@@ -44,17 +26,16 @@ def main():
     if euid != 0:
         parser.error(f"You need to be root! (Expected EUID: 0, received: {euid})")
 
-    validvlans = []
-
+    print("Waiting for VLANs to appear..")
     try:
         if args.timeout:
-            t = threading.Thread(target=capture, kwargs={"interface": args.interface, "validvlans": validvlans})
+            t = threading.Thread(target=sniff, kwargs={"iface": args.interface, "prn":capture})
             t.daemon = True
             t.start()
             print(f"Setting timeout: {args.timeout}s")
             time.sleep(args.timeout)
         else:
-            capture(args.interface, validvlans)
+            sniff(iface=args.interface, prn=capture)
     except:
         print("")
 
